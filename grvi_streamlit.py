252e90a965996579d990131b29ea051188be8ce8
Eitan Fass <90314922+eitanfass@users.noreply.github.com>
2023-01-07 16:11:44 +0200

    Update grvi_streamlit.py

--- grvi_streamlit.py
+++ grvi_streamlit.py
@@ -145,15 +145,32 @@ if app_mode == 'GRVI an Image':
         d_mask=img.copy()
         d_mask[:,:,0],d_mask[:,:,1],d_mask[:,:,2]=mask,mask,mask
         masked_img=img*d_mask
-        
+        # define figure size: width 10 and height 15
+        plt.figure(figsize=(10, 15))
+
+        # subplot for the RGB
+        ax1 = plt.subplot(121, title='Original RGB')
+        im1 = ax1.imshow(img) 
+
+        # subplot for the index
+        ax2 = plt.subplot(122, title=f'{index_name} index, mean={np.nanmean(index):.3f}') # notice the position, and the title
+        cmap=matplotlib.cm.get_cmap('Spectral_r',10)#set colormap to Spectral
+        im2 = ax2.imshow(index*255,cmap=cmap)#show index in Spectral colormap
+        #im2.set_clim(vmax=index.max(), vmin=index.min())# set min max values of color to max and min values of index img
+
+        # add colorbar only to the image on the right
+        divider = make_axes_locatable(ax2)
+        colorbar_ax = divider.append_axes("right", size="5%", pad=0.05)  
+        plt.colorbar(im2, cax=colorbar_ax);
+
 
         # Show the indexed image, mask, and original image side by side
         #show_index(image, index)
-        #st.pyplot()
+        st.pyplot()
         
  
-        st.image(image, caption='Original image', use_column_width=True)
-        st.image(index, caption='Indexed Image', use_column_width=True)
+#         st.image(image, caption='Original image', use_column_width=True)
+#         st.image(index, caption='Indexed Image', use_column_width=True)
        
         st.image(masked_img, caption='Masked Image', use_column_width=True)
         
