54bf3e3984e774cdc9ba7fd40bc19d24dad0cdc4
Eitan Fass <90314922+eitanfass@users.noreply.github.com>
2023-01-07 18:31:28 +0200

    Update grvi_streamlit.py

--- grvi_streamlit.py
+++ grvi_streamlit.py
@@ -187,7 +187,7 @@ if app_mode == 'GRVI an Image':
 #         st.image(image, caption='Original image', use_column_width=True)
 #         st.image(index, caption='Indexed Image', use_column_width=True)
         mask_thresh= mask_threshold = st.slider('Mask sensetivity', -1.0, 1.0, 0.0, 0.02)# asks for input from the user
-        if st.button('What is this?'):
+        if st.button('What is this? '):
           # Display the text as markdown when the button is clicked
           st.markdown('''Adjusting the mask threshold changes the sensitivity of the mask. A high threshold value results in a more sensitive mask, while a low threshold value results in a less sensitive mask.''')
         # Create a binary mask from the index using the mean value as the threshold
